cb1b0b92efaaab7b2f54091cc82e80654054c07a
Antony Maia <antonymaiati@gmail.com>
2020-11-22 20:54:03 -0300

    Crud do Usuario

--- src/main/java/com/elcoma/api/resources/UsuarioResource.java
+++ src/main/java/com/elcoma/api/resources/UsuarioResource.java
@@ -5,14 +5,12 @@ import com.elcoma.api.repositories.UsuarioRepository;
 import com.elcoma.api.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
 
 //Anotação usada para especificar que essa classse é o restController
 @RestController
@@ -30,5 +28,25 @@ public class UsuarioResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}").buildAndExpand(usuario.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Usuario> findById(@PathVariable Integer id){
+
+        Usuario usuario = service.findById(id);
+        return ResponseEntity.ok().body(usuario);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<Usuario>> findAll(){
+        List<Usuario> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@Valid @RequestBody Usuario usuario, @PathVariable Integer id){
+        usuario.setId(id);
+        usuario = service.update(usuario);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/elcoma/api/services/UsuarioService.java
+++ src/main/java/com/elcoma/api/services/UsuarioService.java
@@ -2,8 +2,15 @@ package com.elcoma.api.services;
 
 import com.elcoma.api.domain.Usuario;
 import com.elcoma.api.repositories.UsuarioRepository;
+import com.elcoma.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.Optional;
 
 // Anotação usada para especificar que essa classe é um service, aqui é onde se implementa a regra de negocioo
 @Service
@@ -17,5 +24,27 @@ public class UsuarioService {
         usuario.setId(null);
         usuario = repository.save(usuario);
         return usuario;
+    }
+
+    public Usuario findById(Integer id){
+        Optional<Usuario> usuario = repository.findById(id);
+        return usuario.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado: Id: " + id + ", Tipo: " + Usuario.class.getName()));
+    }
+
+    public List<Usuario> findAll(){
+        return repository.findAll();
+    }
+
+
+    public Usuario update(Usuario usuario){
+        Usuario newUsuario = findById(usuario.getId());
+        newUsuario.setCpf(usuario.getCpf());
+        newUsuario.setEmail(usuario.getEmail());
+        newUsuario.setNascimento(usuario.getNascimento());
+        newUsuario.setSexo(usuario.getSexo());
+        newUsuario.setNome(usuario.getNome());
+        newUsuario.setEndereco(usuario.getEndereco());
+        return repository.save(newUsuario);
     }
 }
